c3254908263c11b0f09e5fe885a6e4d2e950ee6b
一家人xwc <xwc1125@qq.com>
2020-10-11 17:57:03 +0800

    add hash&storagesize

---
+++ types/storagesize.go
@@ -0,0 +1,43 @@
+// description: chain5j-pkg
+// 
+// @author: xwc1125
+// @date: 2020/10/11
+package types
+
+import (
+	"fmt"
+)
+
+// StorageSize is a wrapper around a float value that supports user friendly
+// formatting.
+type StorageSize float64
+
+// String implements the stringer interface.
+func (s StorageSize) String() string {
+	if s > 1000000 {
+		return fmt.Sprintf("%.2f mB", s/1000000)
+	} else if s > 1000 {
+		return fmt.Sprintf("%.2f kB", s/1000)
+	} else {
+		return fmt.Sprintf("%.2f B", s)
+	}
+}
+
+// TerminalString implements log.TerminalStringer, formatting a string for console
+// output during logging.
+func (s StorageSize) TerminalString() string {
+	if s > 1000000 {
+		return fmt.Sprintf("%.2fmB", s/1000000)
+	} else if s > 1000 {
+		return fmt.Sprintf("%.2fkB", s/1000)
+	} else {
+		return fmt.Sprintf("%.2fB", s)
+	}
+}
+
+type WriteCounter StorageSize
+
+func (c *WriteCounter) Write(b []byte) (int, error) {
+	*c += WriteCounter(len(b))
+	return len(b), nil
+}
